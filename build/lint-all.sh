6a912418ef38dcd3b671f7825542008b574638b9
Jacob Greenfield <jacob@jacobgreenfield.me>
2018-12-06 15:12:02 -0500

    Clean environment for xargs

--- build/lint-all.sh
+++ build/lint-all.sh
@@ -3,4 +3,5 @@ set -e
 # List all unignored files.
 { git ls-files --cached --others --exclude-standard || exit $?; } |
 { grep -v 'fixture' || exit $?; } |
-{ xargs "$(dirname "$0")/lint.sh" "$@" || exit $?; }
+# Clean environment for Windows pipeline
+{ env -i xargs "$(dirname "$0")/lint.sh" "$@" || exit $?; }
